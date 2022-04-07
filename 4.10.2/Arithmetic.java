616d0c5c63998db5f1d9a200b875ea8f78b52a53
Александр <alexandr.marchenko.1984@gmail.com>
2022-04-07 12:33:30 +0000

    Update Arithmetic.java

--- 4.10.2/Arithmetic.java
+++ 4.10.2/Arithmetic.java
@@ -1,11 +1,57 @@
 public class Arithmetic {
 
-    private static void main(String[] args) {
+    public static int elemOne = 0;
+    public static int elemTwo = 0;
+    
+    public Arithmetic (String name) {
+        this.elemOne = elemOne;
+        this.elemTwo = elemTwo;
+        System.out.println(name);
+    }
+    
+    public int summ (int elemOne, int elemTwo) {
+        int sum;
+        sum = elemOne + elemTwo;
+        System.out.println("Сумма чисел " + elemOne + " и " + elemTwo + " = " + sum);
+        return sum;
+    }
 
-        int elemOne = 0;
-        int elemTwo = 0;
-
-
-
+    public int multiplication (int elemOne, int elemTwo) {
+        int mult;
+        mult = elemOne * elemTwo;
+        System.out.println("Произведение чисел " + elemOne + " и " + elemTwo + " = " + mult);
+        return mult;
+    }
+    
+    public int maxElem (int elemOne, int elemTwo) {
+        int max;
+        if (elemOne > elemTwo) {
+            System.out.println("Максимальное из чисел " + elemOne + " и " + elemTwo + " = " + elemOne);
+            // max = elemOne;
+        }else if (elemOne < elemTwo) {
+            System.out.println("Максимальное из чисел " + elemOne + " и " + elemTwo + " = " + elemTwo);
+            // max = elemTwo;
+        }else if (elemOne == elemTwo) {
+            // max = elemOne;
+            System.out.println("Числа равны");
+        }
+        max = 0;
+        // System.out.println("Максимальное из чисел " + elemOne + " и " + elemTwo + " = " + max);
+        return max;
+    }
+    
+    public int minElem (int elemOne, int elemTwo) {
+        int min;
+        if (elemOne <  elemTwo) {
+            min = elemOne;
+        } else {
+            min = elemTwo;
+        }
+        if (elemOne == elemTwo) {
+            System.out.println("Числа " + elemOne + " и " + elemTwo + " равны.");
+        } else {
+            System.out.println("Минимальное из чисел " + elemOne + " и " + elemTwo + " = " + min);
+        }
+        return min;
     }
 }
