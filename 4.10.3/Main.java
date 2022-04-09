fc8355c7122c5d9be242271cb7d5c3afd4007dc6
Александр <alexandr.marchenko.1984@gmail.com>
2022-04-09 12:17:34 +0000

    Update Main.java

--- 4.10.3/Main.java
+++ 4.10.3/Main.java
@@ -1,16 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-
         Printer docOne = new Printer();
+        
+        docOne.append("Любой код на Java");
+        docOne.append("Любой код на Java +", "Java+");
+        docOne.print("");
 
-        docOne.append("Любой код на Java", "Java");
-        docOne.append("Любой код на Java + ", " Java + ", 3 );
-        docOne.append("Любой код на Java + + ", " Java + + ", 2 );
-
-        docOne.print("Пример первый.");
-
-
-
-    }
+        docOne.append("Любой код на Java++", "Java+", 4);
+        docOne.append("100", "Java100", 4);
+        docOne.print("");
+    }    
 }
