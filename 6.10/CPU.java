0db68661cd0e2894f826ee1a6253b52ab7ee4c36
Александр <alexandr.marchenko.1984@gmail.com>
2022-05-10 08:49:38 +0700

    Update CPU.java

--- 6.10/CPU.java
+++ 6.10/CPU.java
@@ -1,6 +1,19 @@
-public enum CPU {
-    CPU_FREQUENCY,
-    CPU_CORES,
-    CPU_BRAND,
-    CPU_WEIGHT
+package ru.skillbox;
+
+public class Cpu {
+    final double frequency;
+    final int cores;
+    final String brand;
+    final double weight;
+
+    public Cpu(double frequency, int cores, String brand, double weight) {
+        this.frequency = frequency;
+        this.cores = cores;
+        this.brand = brand;
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
 }
